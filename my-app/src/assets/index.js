45e8a5e8857d2afbcac68535a02ead0723d5e787
neeraj <me.neeraj1455@gmail.com>
2023-02-13 13:25:47 +0530

    initial commit

---
+++ my-app/src/assets/index.js
@@ -0,0 +1,28 @@
+/** @format */
+
+import logo from "./images/logo.png";
+import bannerImg from "./images/bannerImg.png";
+
+import projectOne from "./images/projects/projectOne.jpg";
+import projectTwo from "./images/projects/projectTwo.jpg";
+
+import projectThree from "./images/projects/projectThree.jpeg";
+
+import testimonialOne from "./images/testimonial/testimonialOne.png";
+import testimonialTwo from "./images/testimonial/testimonialTwo.png";
+
+import quote from "./images/testimonial/quote.png";
+
+import contactImg from "./images/contact/contactImg.png";
+
+export {
+  logo,
+  bannerImg,
+  projectOne,
+  projectTwo,
+  projectThree,
+  testimonialOne,
+  testimonialTwo,
+  quote,
+  contactImg,
+};
---
+++ my-app/src/components/features/Features.js
@@ -0,0 +1,59 @@
+/** @format */
+
+import React from "react";
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe } from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
+import Title from "../layouts/Title";
+import Card from "./Card";
+
+function Features() {
+  return (
+    <section
+      id="features"
+      className="w-full pt-10 pb-20  flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
+    >
+      <Title title="Features" des="What I Do" />
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
+        <Card
+          title="Business Stratagy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+        />
+        <Card
+          title="App Development"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<AiFillAppstore />}
+        />
+        <Card
+          title="SEO Optimisation"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<SiProgress />}
+        />
+        <Card
+          title="Mobile Development"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<FaMobile />}
+        />
+        <Card
+          title="UX Design"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<SiAntdesign />}
+        />
+        <Card
+          title="Hosting Websites"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<FaGlobe />}
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Features;
